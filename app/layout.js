a306830e71146baf3d38038e4b6ff8213f4277be
iamShimanto <shimantowebdeveloper@gmail.com>
2025-12-01 23:59:03 +0600

    google tag manager

--- app/layout.js
+++ app/layout.js
@@ -69,8 +69,19 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
         />
+     
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M4K9C7GM');</script>
+
       </head>
       <body>
+      
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M4K9C7GM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
         <Toaster position="top-right" reverseOrder={false} />
         <Navbar />
         {children}
